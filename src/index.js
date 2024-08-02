3869a15cf3595c0c1857ed6169e60d9b8eb14030
polaarts <samuel.angulo_c@mail.udp.cl>
2024-08-02 14:42:51 -0400

    feat: inicializacion del paquete

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { cursos as industrial } from './data/eii'
+import { cursos as informatica } from './data/eit'
+import { cursos as obras } from './data/eoc'
+
+module.exports = { industrial, informatica, obras }
